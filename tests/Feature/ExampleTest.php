64735a229e254d9908f779f3b2ca6775cf823ba3
arfnhd <ariefnur60@gmail.com>
2026-07-08 10:19:35 +0700

    Add Deploy RefreshData

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,14 +2,13 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase; // Pastikan ini ada
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+    use RefreshDatabase; // Baris ini wajib ada agar tiap tes dapat database bersih
+
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
